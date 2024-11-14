97b0855a731b413a965b66e1538045d66e67f310
Shreyas <sm_patil@gatech.edu>
2024-11-14 11:55:37 -0500

    multi-task

---
+++ multi-task/models/mlp.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+from models.layers.layers import MaskedConv2d, MaskedLinear
+
+class mlp(nn.Module):
+    def __init__(self, arch, num_tasks=1, output_dim=2):
+        super(mlp, self).__init__()
+        self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        layers = []
+        if num_tasks>1:
+            arch[0] = arch[0]+num_tasks
+        for i in range(1, len(arch)-1):
+            linear = MaskedLinear(arch[i-1], arch[i])
+            layers += [linear, nn.ReLU(inplace=True)]
+        self.layers = nn.Sequential(*layers)
+
+        self.num_tasks = num_tasks
+        self.output_heads = MaskedLinear(arch[-2], arch[-1])
+        #self.output_heads = nn.ModuleList()
+        #for i in range(arch[-1]):
+        #    linear = MaskedLinear(arch[-2], self.num_tasks)
+        #    self.output_heads.append(linear)
+
+        self.output = nn.Sigmoid()
+    
+    def forward(self, x, task_id, train=True):
+        #x = self.layers(x)
+        #for i in range(len(self.output_heads)):
+        #    head_out = self.output_heads[i](x)
+        #    head_out = (head_out*torch.nn.functional.one_hot(task_id, num_classes=self.num_tasks).to(self.device)).sum(dim=1).view(x.size(0),1)
+        #    if i==0:
+        #        y = head_out
+        #    else:
+        #        y = torch.cat((y, head_out), dim=1)
+        #x = y
+        #x = self.output(x)
+        #print(x)
+        if self.num_tasks>1:
+            x = torch.cat((x, torch.nn.functional.one_hot(task_id, num_classes=self.num_tasks).to(self.device)), dim=1)
+        x = self.layers(x)
+        x = self.output_heads(x)
+        x = self.output(x)
+        return x
+
+   
+    def set_masks(self, weight_mask, bias_mask):
+        i = 0
+        for m in self.modules():
+            if isinstance(m,(MaskedLinear, MaskedConv2d)):
+                m.set_mask(weight_mask[i],bias_mask[i])
+                i = i + 1
---
+++ multi-task/run/multi_task.py
@@ -0,0 +1,109 @@
+import torch
+import pickle as pkl
+
+from utils import train, load
+from models import utils
+import os
+
+def run(args):
+
+    load.set_seed(args.seed)
+    dev = load.load_device(args.gpu)
+    direct_models, direct_results = load.create_directory(args)
+
+    if args.model=='mlp':
+        path = direct_results + args.model + str(args.arch) + str(args.pruning_ratio) + str(args.lr) + str(args.batch_size) + str(args.epochs) + str(args.weight_decay) + str(args.gamma) + str(args.seed) + '_Results' + str(args.dataset_split) + str(args.dataset_split_seed)+ str(args.dataset_noise) + '.pkl'
+    if args.model=='hierarchically_modular':
+        path = direct_results + args.model + str(args.num_modules) + str(args.module_arch) + str(args.topk) + str(args.use_gumbel) + str(args.tau) + str(args.per_sample_routing) + str(args.known_module_inputs) + str(args.known_func_outputs) + str(args.lr) + str(args.lr_module_input) + str(args.batch_size) + str(args.epochs) + str(args.weight_decay) + str(args.gamma) + str(args.seed) + '_Results' + str(args.dataset_split) + str(args.dataset_split_seed) + str(args.dataset_noise) + '.pkl'
+    if args.model=='hierarchically_modular_shared_modules':
+        path = direct_results + args.model + str(args.num_slots) + str(args.num_shared_modules) + str(args.module_arch) + str(args.topk) + str(args.use_gumbel) + str(args.tau) + str(args.per_sample_routing) + str(args.known_module_inputs) + str(args.known_module_locations) + str(args.known_func_outputs) + str(args.lr) + str(args.lr_module_input) + str(args.lr_module_location) + str(args.batch_size) + str(args.epochs) + str(args.weight_decay) + str(args.gamma) + str(args.seed) + '_Results' + str(args.dataset_split) + str(args.dataset_split_seed) + str(args.dataset_noise) + '.pkl'
+    
+    if os.path.exists(path) and os.path.getsize(path) > 0:
+        print('Shreyas:', path)
+    else:
+        train_loader, train_loader2, validation_loader, test_loader = load.load_data(args.dataset_path, args.num_tasks, args.modularity, args.batch_size, args.dataset_noise, args.dataset_split, args.dataset_split_seed)
+        loss = load.load_loss(args.loss)
+        opt, opt_kwargs = load.load_optimizer(args.optimizer)
+
+        if args.model == 'mlp':
+
+            model = load.load_model(args.model)(args.arch, args.num_tasks).to(dev)
+            if args.pruning_ratio > 0.0:
+                weight_mask, bias_mask = utils.rand_prune_masks(model, args.pruning_ratio, dev)
+                model.set_masks(weight_mask, bias_mask)
+            optimizer = opt(model.parameters(), lr=args.lr, weight_decay=args.weight_decay, **opt_kwargs)
+
+        elif args.model == 'hierarchically_modular':
+
+            if args.known_module_inputs:
+
+                module_inp_indices = load.load_routing(args)
+                model = load.load_model(args.model)(args.num_modules, args.module_arch, args.num_tasks, args.modularity[0], args.modularity[-1], args.topk, args.use_gumbel, args.per_sample_routing, args.tau, args.known_module_inputs, module_inp_indices).to(dev)
+                optimizer = opt([
+                    {'params': model.module_mlps.parameters(), 'lr': args.lr}
+                    ], weight_decay=args.weight_decay, **opt_kwargs)
+
+            else:
+
+                model = load.load_model(args.model)(args.num_modules, args.module_arch, args.num_tasks, args.modularity[0], args.modularity[-1], args.topk, args.use_gumbel, args.per_sample_routing, args.tau).to(dev)
+                optimizer = opt([
+                    {'params': model.module_inp_embeddings, 'lr': args.lr_module_input},
+                    {'params': model.module_mlps.parameters(), 'lr': args.lr}
+                    ], weight_decay=args.weight_decay, **opt_kwargs)
+
+        elif args.model == 'hierarchically_modular_shared_modules':
+
+            if args.known_module_inputs and args.known_module_locations:
+
+                module_input_indices, slot_module_indices = load.load_routing(args)
+                model = load.load_model(args.model)(args.num_shared_modules, args.num_slots, args.module_arch, args.num_tasks, args.modularity[0], args.modularity[-1], args.topk, args.use_gumbel, args.per_sample_routing, args.tau, args.known_module_inputs, args.known_module_locations, module_input_indices, slot_module_indices).to(dev)
+                optimizer = opt([
+                    {'params': model.module_mlps.parameters(), 'lr': args.lr}
+                    ], weight_decay=args.weight_decay, **opt_kwargs)
+
+            elif args.known_func_outputs:
+
+                module_input_indices, slot_module_indices = load.load_routing(args)
+                model = load.load_model(args.model)(args.num_shared_modules, args.num_slots, args.module_arch, args.num_tasks, args.modularity[0], args.modularity[-1], args.topk, args.use_gumbel, args.per_sample_routing, args.tau, args.known_module_inputs, args.known_module_locations, module_input_indices, slot_module_indices, args.known_func_outputs).to(dev)
+                optimizer = opt([
+                    {'params': model.module_inp_embeddings[:-1], 'lr': args.lr_module_input},
+                    {'params': model.module_loc_embeddings, 'lr': args.lr_module_location},
+                    {'params': model.module_mlps.parameters(), 'lr': args.lr}
+                    ], weight_decay=args.weight_decay, **opt_kwargs)
+        
+            else:    
+            
+                model = load.load_model(args.model)(args.num_shared_modules, args.num_slots, args.module_arch, args.num_tasks, args.modularity[0], args.modularity[-1], args.topk, args.use_gumbel, args.per_sample_routing, args.tau).to(dev)
+                optimizer = opt([
+                    {'params': model.module_inp_embeddings, 'lr': args.lr_module_input},
+                    {'params': model.module_loc_embeddings, 'lr': args.lr_module_location},
+                    {'params': model.module_mlps.parameters(), 'lr': args.lr}
+                    ], weight_decay=args.weight_decay, **opt_kwargs)
+
+        scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=args.gamma, last_epoch=- 1)
+        (model, validation_loss, train_loss, test_loss, validation_accuracy, train_accuracy, test_accuracy) = train.train_network(model, loss, optimizer, train_loader, train_loader2, validation_loader, test_loader, dev, args.epochs, scheduler)
+
+        results = []
+        results.append(train_loss)
+        results.append(validation_loss)
+        results.append(test_loss)
+        results.append(train_accuracy)
+        results.append(validation_accuracy)
+        results.append(test_accuracy)
+        
+
+        if args.model=='mlp':
+
+            with open(direct_results + args.model + str(args.arch) + str(args.pruning_ratio) + str(args.lr) + str(args.batch_size) + str(args.epochs) + str(args.weight_decay) + str(args.gamma) + str(args.seed) + '_Results' + str(args.dataset_split) + str(args.dataset_split_seed)+ str(args.dataset_noise) + '.pkl', "wb") as fout:
+                pkl.dump(results, fout, protocol=pkl.HIGHEST_PROTOCOL)
+
+
+        if args.model=='hierarchically_modular':
+
+            with open(direct_results + args.model + str(args.num_modules) + str(args.module_arch) + str(args.topk) + str(args.use_gumbel) + str(args.tau) + str(args.per_sample_routing) + str(args.known_module_inputs) + str(args.known_func_outputs) + str(args.lr) + str(args.lr_module_input) + str(args.batch_size) + str(args.epochs) + str(args.weight_decay) + str(args.gamma) + str(args.seed) + '_Results' + str(args.dataset_split) + str(args.dataset_split_seed) + str(args.dataset_noise) + '.pkl', "wb") as fout:
+                pkl.dump(results, fout, protocol=pkl.HIGHEST_PROTOCOL)
+
+        if args.model=='hierarchically_modular_shared_modules':
+
+            with open(direct_results + args.model + str(args.num_slots) + str(args.num_shared_modules) + str(args.module_arch) + str(args.topk) + str(args.use_gumbel) + str(args.tau) + str(args.per_sample_routing) + str(args.known_module_inputs) + str(args.known_module_locations) + str(args.known_func_outputs) + str(args.lr) + str(args.lr_module_input) + str(args.lr_module_location) + str(args.batch_size) + str(args.epochs) + str(args.weight_decay) + str(args.gamma) + str(args.seed) + '_Results' + str(args.dataset_split) + str(args.dataset_split_seed) + str(args.dataset_noise) + '.pkl', "wb") as fout:
+                pkl.dump(results, fout, protocol=pkl.HIGHEST_PROTOCOL)
